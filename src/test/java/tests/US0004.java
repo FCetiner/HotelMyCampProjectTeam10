adcb46411dc962f18702bbfef9b3ae7417dc5d88
Hadi ARICI <hayatiyasamak34@gmail.com>
2022-02-12 16:39:22 +0200

    Merge remote-tracking branch 'origin/main'

--- src/test/java/tests/US0004.java
+++ src/test/java/tests/US0004.java
@@ -1,8 +1,40 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class US0004 {
 
+    /*
+      * TC_001 Hoetel Sayafasina Gidelim https://www.hotelmycamp.com/
+      * TC_002 Yönetici Olarak
+           *Logini Tikla  Kullanici adi  ve sifre degerlerini yazdirip, giris butonuna basin ve giris yapin
+
+      * TC_003 Yönetici olarak
+          * Logini Tikla  Kullanici adi  ve sifre degerlerini yazdirip, giris butonuna basin ve giris yapin
+          * Otel Management Linkini Tikla ,  Otel List Linkini Tikla, Add Otel Linkini Tikla,
+          * Create Hotel formunu gorunuru oldugunu test et.
+
+      * TC_004 Yönetici olarak
+          * Logini Tikla  Kullanici adi  ve sifre degerlerini yazdirip, giris butonuna basin ve giris yapin
+          * Otel Management Linkini Tikla ,  Otel List Linkini Tikla, Add Otel Linkini Tikla,
+          * Create Hotel formunu daki Code,Name,Adrress, Phone,Email,IDGroup ilgili alanlari doldur
+          * Save Buttunan basıp kaydet.
+
+      *TC_005 Yönetici olarak
+        * Logini Tikla  Kullanici adi  ve sifre degerlerini yazdirip, giris butonuna basin ve giris yapin
+        * Otel Management Linkini Tikla ,  Otel List Linkini Tikla,
+        * (LIST OF HOTELS tablosunda, Code,Name,Adrress, Phone,Email yazdigin verilerden herhangi birini gir.
+        * Search Buttununa tikla
+        * Kayit olup olmadigini test et.
+
+
+     */
+
+
+
+    @Test
     public void test01(){
+
 
     }
 }
