c924f65047a0de6d6d29b53d6c3bd180f005e722
Ferhat <fcetinerr@gmail.com>
2022-02-13 17:52:48 +0300

    c02

--- src/test/java/tests/us_0001/TC_01.java
+++ src/test/java/tests/us_0001/TC_01.java
@@ -1,6 +1,7 @@
 package tests.us_0001;
 
 import org.testng.annotations.Test;
+import pages.HMCPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
@@ -8,7 +9,7 @@ import utilities.ReusableMethods;
 public class TC_01 {
     @Test
     public void test() {
-        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
-        ReusableMethods.waitForPageToLoad(5);
+        HMCPage hmcPage=new HMCPage();
+        hmcPage.anasayfaGiris();
     }
 }
