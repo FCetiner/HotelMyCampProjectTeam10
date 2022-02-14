e5109ff7fed83f7af4146c9c1a75b9c04e54a7bb
Ferhat <fcetinerr@gmail.com>
2022-02-15 00:28:19 +0300

    Yeni Page Classlari eklendi

---
+++ src/test/java/pages/HotelRoomReservationPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HotelRoomReservationPage {
+    public HotelRoomReservationPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+//***************Gokhan Bey-Hacer Hanim**************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
