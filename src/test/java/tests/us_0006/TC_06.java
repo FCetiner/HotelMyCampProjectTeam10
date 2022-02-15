169a4204780d5bdeb9aa53367920f8bdf7eedabc
Burak <sarisahinburak22@gmail.com>
2022-02-15 23:39:29 +0100

    b2

---
+++ src/test/java/tests/us_0006/TC_06.java
@@ -0,0 +1,28 @@
+package tests.us_0006;
+
+public class TC_06 {
+    //1-Yonetici olarak ana sayfaya gidin
+    //2-Login yazisina tiklayin
+    //3-Yonetici olarak gecerli bir username girin
+    //4-Yonetici olarak gecerli bir password girin
+    //5-Login butonuna tiklayin
+    //6-Hotel management yazisina tiklayin
+    //7-Hotel rooms yazisina tiklayin
+    //8-Add HotelRoom butonuna tiklayin
+    //9-ID Hotel Dropdown dan Hotel secn
+    //10- Code box una tiklayip gecerli biir numara girin
+    //11-Name box una tiklayip oda ismini girin
+    //12-Location box una tiklayip gecerli bir adres girin
+    //13-Description kısmına birseyler yazin
+    //14-Drag and drop butonlarina basarak price degeri girin
+    //15-Room Type dropDown dan oda tipini belirleyin
+    //16-Max Adult count box undan adult sayisi girin
+    //17-Max Children count  box undan cocuk sayisi girin
+    //18-Approved kutusuna tıklayin
+    //19-Save butonuna basin
+    //20-"HotelRoom was inserted successfully" yazisnin gorunurlugunu test edin
+    //21-"OK" butonuna tiklatin
+    //22-Sayfanın altındaki ok tuşuna basılarak sayfanın basina gidin
+    //23-"Create Hotelroom" yazısının görünürlüğünu test edin
+
+}
