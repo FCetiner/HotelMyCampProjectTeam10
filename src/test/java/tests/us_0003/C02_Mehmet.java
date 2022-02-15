c7da578413fba904fe0a7d82594619af5605eb63
mehmet tapan <m.cagritapan@gmail.com>
2022-02-15 13:54:58 +0300

    v10

--- src/test/java/tests/us_0003/C02_Mehmet.java
+++ src/test/java/tests/us_0003/C02_Mehmet.java
@@ -13,5 +13,6 @@ public class C02_Mehmet {
         System.out.println("deneme2");
         System.out.println("deneme3");
         System.out.println("deneme4");
+        System.out.println("denme5");
     }
 }
