53eb74cbe5e73296710056e7ca440824a460ba5e
Deniz Unal <unaldeniz676@gmail.com>
2022-02-16 20:46:29 +0300

    v4

--- src/test/java/tests/us_0007/TC_04.java
+++ src/test/java/tests/us_0007/TC_04.java
@@ -14,58 +14,6 @@ import utilities.Driver;
 public class TC_04 {
 
 
- /*Step 1
- *Kullanıcı url yi girer */
-
-    @Test
-    public void test01(){
-        HMCPage hmcPage =new HMCPage();
-        SoftAssert softAssert=new SoftAssert();
-        hmcPage.anasayfaGiris();
-    }
-    /*Step 2
-     **Geçerli bilgilerle Log-in yapılır
-     */
-    @Test
-    public void test02(){
-        HMCPage hmcPage =new HMCPage();
-        SoftAssert softAssert=new SoftAssert();
-        hmcPage.girisYap();
-    }
-
-    /*Step 3
-     **"Hotel Management" sekmesinden "Hotelş Rooms" butonuna tıklanır  */
-    @Test
-    public void test03(){
-        HMCPage hmcPage = new HMCPage();
-        hmcPage.hotelManagementLinki.click();
-        HotelRoomsPage roomsPage = new HotelRoomsPage();
-        roomsPage.hotelRoomsLink.click();
-    }
-
-    @Test
-    public void test04(){
-        HotelRoomsPage roomsPage=new HotelRoomsPage();
-        roomsPage.detailsLink.click();
-    }
-
-    @Test
-    public void test05(){
-        HotelRoomsPage roomsPage=new HotelRoomsPage();
-        roomsPage.hotelIdbox.click();
-        Select select = new Select(roomsPage.hotelIdbox);
-        select.getFirstSelectedOption();
-
-        Actions actions=new Actions(Driver.getDriver());
-        Faker faker=new Faker();
-
-        actions.click(roomsPage.codeBox).sendKeys((WebElement) faker.code()).
-                sendKeys(Keys.TAB).sendKeys(faker.name().name())
-                .sendKeys(Keys.TAB).sendKeys(faker.address().city())
-                .sendKeys(Keys.TAB).sendKeys(faker.toString())
-                .sendKeys(Keys.TAB).sendKeys(faker.commerce().price()).perform();
-
-
 
     }
 
