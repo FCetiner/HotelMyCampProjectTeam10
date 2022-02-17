e012a4a37bf18bae8d0466f556ff675c7b89a6c5
Kader <kader.demirkoparann@gmail.com>
2022-02-17 10:19:56 +0300

    degisiklikler

---
+++ src/test/java/tests/us_00010/TC_05.java
@@ -0,0 +1,15 @@
+package tests.us_00010;
+
+import org.testng.annotations.Test;
+
+public class TC_05 {
+    @Test
+    public void testCase5(){
+        //        Kullanici ana sayfaya gider
+        //login butonuna tıklar
+        //"User Menu" texti altindaki "Reservations" buttonu tiklar
+        //onların görülebildigini test eder
+
+
+    }
+}
