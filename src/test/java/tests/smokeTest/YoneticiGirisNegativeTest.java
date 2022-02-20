8f8645d1646f55b9b1841299c4e0ca1b3689e95e
Funda Yaylagul <fundafunda0607@gmail.com>
2022-02-20 19:56:35 +0300

    sonversion

--- src/test/java/tests/smokeTest/YoneticiGirisNegativeTest.java
+++ src/test/java/tests/smokeTest/YoneticiGirisNegativeTest.java
@@ -4,39 +4,50 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HMCPage;
 import utilities.ConfigReader;
+import utilities.TestBaseRapor;
 
-public class YoneticiGirisNegativeTest {
+public class YoneticiGirisNegativeTest extends TestBaseRapor {
+
     HMCPage hmcPage =new HMCPage();
+
     @Test
     public void yanlisUserNameTesti(){
 
 
+        extentTest=extentReports.createTest("Negatif smoke test","Yönetici yanliş username ile giriş yapilamadi");
         hmcPage.anasayfaGiris();
+        extentTest.info("Url' e giriş yapildi");
         hmcPage.ilkLoginLinki.click();
         hmcPage.usernameBox.sendKeys(ConfigReader.getProperty("HMCWrongUsername"));
         hmcPage.passwordBox.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
         hmcPage.loginButonu.click();
         Assert.assertTrue(hmcPage.girisYapilamadiYazisi.isDisplayed());
-
+        extentTest.pass("Yanlis usurname testi pass");
     }
+    @Test
     public void yanlisPasswordTest(){
 
+        extentTest=extentReports.createTest("Negatif smoke test","Yönetici yanliş password ile giriş yapilamadi");
         hmcPage.anasayfaGiris();
+        extentTest.info("Url' e giriş yapildi");
         hmcPage.ilkLoginLinki.click();
         hmcPage.usernameBox.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
         hmcPage.passwordBox.sendKeys(ConfigReader.getProperty("HMCWrongPassword"));
         hmcPage.loginButonu.click();
         Assert.assertTrue(hmcPage.girisYapilamadiYazisi.isDisplayed());
-
+        extentTest.pass("Yanlis password testi pass");
     }
+    @Test
     public void yanlisUserPasswordTest(){
 
+        extentTest=extentReports.createTest("Negatif smoke test","Yönetici yanliş username ve yanlis password ile giriş yapilamadi");
         hmcPage.anasayfaGiris();
+        extentTest.info("Url' e giriş yapildi");
         hmcPage.ilkLoginLinki.click();
         hmcPage.usernameBox.sendKeys(ConfigReader.getProperty("HMCWrongUsername"));
         hmcPage.passwordBox.sendKeys(ConfigReader.getProperty("HMCWrongPassword"));
         hmcPage.loginButonu.click();
         Assert.assertTrue(hmcPage.girisYapilamadiYazisi.isDisplayed());
-
+        extentTest.pass("Yanlis usurname ve password testi pass");
     }
 }
--- src/test/java/tests/smokeTest/YoneticiGirisiPozitifTest.java
+++ src/test/java/tests/smokeTest/YoneticiGirisiPozitifTest.java
@@ -4,18 +4,26 @@ import org.testng.Assert;
 import pages.HMCPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.TestBaseRapor;
 
-public class YoneticiGirisiPozitifTest {
+public class YoneticiGirisiPozitifTest extends TestBaseRapor {
 
     public void yoneticigirisYap() {
-
-        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
         HMCPage hmcPage =new HMCPage();
+        extentTest=extentReports.createTest("Pozitif smoke test","Yönetici başarili giriş yapabildi");
+        hmcPage.anasayfaGiris();
+        extentTest.info("Url giriş yapildi");
+
         hmcPage.ilkLoginLinki.click();
+        extentTest.info("İlk login clik yapildi");
         hmcPage.usernameBox.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
+        extentTest.info("Username girişi yapildi");
         hmcPage.passwordBox.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
+        extentTest.info("Password girişi yapildi");
         hmcPage.loginButonu.click();
         Assert.assertTrue(hmcPage.managerDrapDown.isDisplayed());
+        extentTest.pass("Yonetici başarili giriş yapabilmiştir");
+
 
     }
 }
