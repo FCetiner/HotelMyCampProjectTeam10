25a41c468fc38fe491fc950ae10461d4c22fc9f8
Funda Yaylagul <fundafunda0607@gmail.com>
2022-02-22 20:19:28 +0300

    snn

--- src/test/java/tests/us_0006/TC_01.java
+++ src/test/java/tests/us_0006/TC_01.java
@@ -3,31 +3,26 @@ package tests.us_0006;
 
 import org.testng.annotations.Test;
 import pages.HMCPage;
+import utilities.Driver;
 import utilities.ReusableMethods;
 import utilities.TestBaseRapor;
 
 public class TC_01 extends TestBaseRapor {
 
-    HMCPage hmcPage =new HMCPage();
+    HMCPage hmcPage;
 
-    //1-Yonetici olarak ana sayfaya gidin
     @Test
     public void test01() {
+        hmcPage =new HMCPage();
         extentTest=extentReports.createTest("TC_01","\"Log in\" buttonuna tiklayarak, giris yapabilmeli.");
         hmcPage.anasayfaGiris();
-
         extentTest.info("Url ye gidildi");
-    }
-
-    //2-Login yazisina tiklayin
-    @Test
-    public void test02() {
-        extentTest=extentReports.createTest("TC_01","\"Log in\" buttonuna tiklayarak, giris yapabilmeli.");
-        hmcPage.anasayfaGiris();
         ReusableMethods.waitFor(4);
         hmcPage.ilkLoginLinki.click();
         extentTest.info("\"Log in\" linkine tiklanildi");
-
+        Driver.closeDriver();
     }
+
+
 
 }
